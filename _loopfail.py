5288ecc88c57517f999d82062595e3544c700f57
mck <451143003@qq.com>
2024-07-25 23:17:34 +0800

    [optimize]

--- _loopfail.py
+++ _loopfail.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import time
 
 if __name__ == '__main__':
     try:
         cmd = ' '.join(sys.argv[1:])
         while 0 != os.system(cmd):
             print("Error, retrying...")
+            time.sleep(2)
         print("Done")
     except KeyboardInterrupt:
         exit(-1)
